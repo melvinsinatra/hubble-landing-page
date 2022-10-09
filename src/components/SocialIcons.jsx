56fcc604665e053a9d502446ce94491cfb55d1f9
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 17:31:05 +0700

    add SocialIcons.jsx

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa'
+
+const SocialIcons = () => {
+  return (
+    <div>
+      <li>
+        <a href="https://twitter.com"><FaTwitter/></a>
+        <a href="https://facebook.com"><FaFacebook/></a>
+        <a href="https://linkedin.com"><FaLinkedin/></a>
+      </li>
+    </div>
+  )
+}
+
+export default SocialIcons
