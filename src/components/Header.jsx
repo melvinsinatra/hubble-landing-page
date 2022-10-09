de42b5ee637e54275419e5254d82a8952f7b9b89
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 14:56:26 +0700

    fix minor issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
 
 const Header = () => {
   return (
-    <Header>
+    <StyledHeader>
       <h1>Hubble</h1>
-    </Header>
+    </StyledHeader>
   )
 }
 
