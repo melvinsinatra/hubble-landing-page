6d26feecdcf417c566d317c790f5f83712892a40
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 17:27:39 +0700

    add styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from './styles/Container.styled';
 import { Flex } from './styles/Flex.styled';
+import { StyledFooter } from './styles/Footer.styled'
 
 const Footer = () => {
 	return (
