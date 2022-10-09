caf0aeecee7ba4e0584e9005538bf7cd4d3304b7
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 15:04:48 +0700

    add Nav and Logo

--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -4,3 +4,14 @@ export const StyledHeader = styled.header`
   background-color: ${({theme}) => theme.colors.header};
   padding: 2.5em 0;
 `
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2.5em;
+`
+
+export const Logo = styled.img`
+  
+`
