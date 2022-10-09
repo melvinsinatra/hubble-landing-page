f9d1a06db0dab8d775f1dd4a8a530bd029af2cbb
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 14:35:05 +0700

    add ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Container } from './components/styles/Container.styled'
 
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
 function App() {
 
   return (
-    <Container>
-      <h1>Hello world</h1>
-    </Container>
+      <ThemeProvider theme = {theme}>
+        <Header/>
+            <Container>
+        <h1>Hello world</h1>
+            </Container>
+      </ThemeProvider>
   )
 }
 
