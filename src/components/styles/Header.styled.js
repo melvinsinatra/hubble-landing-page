a441dc0b8b91d0c2997e0ddadb2e46254a52e774
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 15:41:06 +0700

    add media query to Image

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,29 +1,33 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-  background-color: ${({theme}) => theme.colors.header};
-  padding: 2.5em 0;
-`
+	background-color: ${({ theme }) => theme.colors.header};
+	padding: 2.5em 0;
+`;
 
 export const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 2.5em;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 2.5em;
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobile}) {
-    flex-direction: column;
-  }
-`
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+		flex-direction: column;
+	}
+`;
 
 export const Logo = styled.img`
-@media(max-width: ${({theme}) => theme.breakpoints.mobile}) {
-  margin-bottom: 2.5em;
-}
-`
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+		margin-bottom: 2.5em;
+	}
+`;
 
 export const Image = styled.img`
-  width: 100%;
-  max-width: 375px;
-  margin-left: 2.5em;
-`
+	width: 100%;
+	max-width: 375px;
+	margin-left: 2.5em;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+		margin" 2.5em 0 1.875em;
+	}
+`;
