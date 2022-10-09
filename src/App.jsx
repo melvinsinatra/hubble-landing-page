4b90d7084586419d87bdcb3f84e726b8a077f597
Melvin Sinatra <melvin.sinatra2002@gmail.com>
2022-10-09 15:17:49 +0700

    add minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { Container } from './components/styles/Container.styled';
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { StyledHeader } from './styles/Header.styled'
+import React from 'react';
+import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Flex } from './styles/Flex.styled';
+import { Button } from './styles/Button.styled';
 
 const Header = () => {
-  return (
-    <StyledHeader>
-      <h1>Hubble</h1>
-    </StyledHeader>
-  )
-}
+	return (
+		<StyledHeader>
+			<Container>
+				<Nav>
+					<Logo src="./images/logo.svg" alt="" />
+					<Button>Try It Free</Button>
+				</Nav>
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+          </div>
+        </Flex>
+			</Container>
+		</StyledHeader>
+	);
+};
 
-export default Header
+export default Header;
